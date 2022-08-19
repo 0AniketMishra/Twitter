a1a751ab441c37614440027a7b2c323ae37791b5
ConnectionsLLC <0aniketmishra@gmail.com>
2022-08-19 13:39:52 +0530

    Login page updated!

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import { auth, provider } from '../firebase'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import styles from '../styles/nav.module.css'
 
 function login() {
   
@@ -10,12 +11,16 @@ function login() {
     signInWithPopup(auth, provider)
   }
   return (
-    <div>
+    <div className={styles.bg}>
      <Head>
         <title>Login | Twitter 2.0</title>
      </Head>
+    <div className={styles.center3}>
+    
 
-     <button className='ml-10' onClick={signIn}>Sign in with Google</button>
+        <button className='p-2 font-bold text-white text-xl bg-purple-800 rounded-lg' onClick={signIn}>Sign In Using Google</button>
+
+    </div>
     </div>
   )
 }
